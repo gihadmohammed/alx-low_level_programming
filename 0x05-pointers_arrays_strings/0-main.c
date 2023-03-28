a5b5274aeb42b979f7228efd81443f3c4411eeef
gihadmohammed <gihadrashid>
2023-03-28 02:03:34 +0200

    3

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -9,7 +9,6 @@
 int main(void)
 {
     int n;
-
     n = 402;
     printf("n=%d\n", n);
     reset_to_98(&n);
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,1 +1,1 @@
-C - More functions, more nested loops
+C - Pointers, arrays and strings
